311ed491ca73ec058037d7af977ffe4dc3831762
Junior Santos <josejunior.engtec@gmail.com>
2022-05-04 21:48:13 -0300

    alteracao na baseUrl para o link da api no heroku

--- src/app/services/correntista.service.ts
+++ src/app/services/correntista.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'https://santander-bankline-api.herokuapp.com/';
 
 
 @Injectable({
